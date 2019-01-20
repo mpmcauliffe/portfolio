db87a7e01c040b5a83ee2a4caa1c01155edc5ab1
mpmcauliffe <mcauliffe99@gmail.com>
2019-01-19 20:18:39 -0500

    add honeycomb architecture

--- client/src/react-components/CanvasPrime.js
+++ client/src/react-components/CanvasPrime.js
@@ -1,13 +1,12 @@
 import React, { Fragment } from 'react'
 import { Honeycomb } from './components'
-import ThreeContainer from '../assets/playground/Header'
+//import ThreeContainer from '../assets/playground/Header'
 
 
 const CanvasPrime = () => {
 
     return (
         <Fragment>
-            <ThreeContainer />
             <Honeycomb />    
         </Fragment>
     )
