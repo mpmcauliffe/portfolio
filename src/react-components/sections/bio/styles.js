451a9683b183ad3348d40f73db4de44f930b66c4
mpmcauliffe <mcauliffe99@gmail.com>
2020-06-01 02:55:29 -0600

    2nd deploy

--- src/react-components/sections/bio/styles.js
+++ src/react-components/sections/bio/styles.js
@@ -34,7 +34,7 @@ export const ListOutContainer = styled.div`
     background: #C7D2D5;
     border: .1rem solid #05090A;
     opacity: .7;
-    border-radius: 3rem;
+    /* border-radius: 3rem; */
 
     @media (max-width: 1280px) {
         margin-top: 5rem;
